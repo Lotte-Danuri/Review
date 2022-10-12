62698c67575bbc9717ad0620228e94b3737cdb16
SBHbro <ssd018@gamil.com>
2022-10-12 21:25:52 +0900

    Fix : CrossOrigin 변경 - CrossOrigin 수정

--- src/main/java/com/danuri/review/controller/ReplyController.java
+++ src/main/java/com/danuri/review/controller/ReplyController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reply")
+@CrossOrigin("*")
 public class ReplyController {
 
     private final ReplyService replyService;
--- src/main/java/com/danuri/review/controller/ReviewController.java
+++ src/main/java/com/danuri/review/controller/ReviewController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 @RestController()
 @RequiredArgsConstructor
 @RequestMapping("/review")
+@CrossOrigin("*")
 public class ReviewController {
 
     private final ReviewService reviewService;

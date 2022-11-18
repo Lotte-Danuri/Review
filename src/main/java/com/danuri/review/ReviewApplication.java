eca1b46aa7ffa6bb754d0508fdbe39416aac137a
SBHbro <ssd018@gamil.com>
2022-11-18 21:35:13 +0900

    (FEAN-230) Fix : Timezone 수정

--- src/main/java/com/danuri/review/ReviewApplication.java
+++ src/main/java/com/danuri/review/ReviewApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
 public class ReviewApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
